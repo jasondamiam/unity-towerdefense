5ea286cf58891ad120971472ec901fdc15a47df8
jasondamiam <144017862+jasondamiam@users.noreply.github.com>
2024-05-29 11:01:50 +0200

    MONEYYYYYYYY!!!

---
+++ My project (1)/Assets/Scripts/money.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class money : MonoBehaviour
+{
+    public TextMeshProUGUI amountonScreen;
+    public float startAmount;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        amountonScreen.SetText("$" + startAmount.ToString());
+    }
+}
