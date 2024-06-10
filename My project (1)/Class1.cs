91d71c6861cb574eb25ef7ed62fda2d83f748a94
thiadmargo <144018035+thiadmargo@users.noreply.github.com>
2024-06-10 10:21:47 +0200

    aa

---
+++ My project (1)/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTower : MonoBehaviour
+{
+    private HealthBar healthBarInstance;  // Verander de naam van het veld om conflicten te voorkomen
+    public Slider healthBarSlider;
+
+    private void Start()
+    {
+        healthBarInstance = HealthBar.GetInstance();
+        if (healthBarSlider != null)
+        {
+            healthBarSlider.maxValue = healthBarInstance.MaxHealth;
+            healthBarSlider.value = healthBarInstance.CurrentHealth;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        healthBarInstance.TakeDamage(amount);
+        if (healthBarSlider != null)
+        {
+            healthBarSlider.value = healthBarInstance.CurrentHealth;
+        }
+        if (!healthBarInstance.IsAlive())
+        {
+            Debug.Log("EndTower is destroyed!");
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        healthBarInstance.Heal(amount);
+        if (healthBarSlider != null)
+        {
+            healthBarSlider.value = healthBarInstance.CurrentHealth;
+        }
+    }
+
+    public HealthBar HealthBar
+    {
+        get { return healthBarInstance; }
+    }
+}
---
+++ My project (1)/Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class program : MonoBehaviour
+{
+    void Start()
+    {
+        EndTower tower1 = new GameObject("Tower1").AddComponent<EndTower>();
+        tower1.healthBarSlider = FindObjectOfType<Slider>();  // Stel de slider in voor demonstratiedoeleinden
+        EndTower tower2 = new GameObject("Tower2").AddComponent<EndTower>();
+        tower2.healthBarSlider = FindObjectOfType<Slider>();  // Stel de slider in voor demonstratiedoeleinden
+
+        tower1.TakeDamage(30);
+        tower2.TakeDamage(50);
+        tower1.Heal(20);
+
+        Debug.Log($"Health after actions: {tower1.HealthBar.CurrentHealth}/{tower1.HealthBar.MaxHealth}");
+    }
+}
